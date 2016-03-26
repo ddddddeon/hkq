8466ec3e70568000a90eaea92e96af5261fb20fe
ddddddeon <chris@sweetiq.com>
2016-03-26 03:36:29 -0400

    separate functionality into separate functions

--- server.js
+++ server.js
@@ -13,7 +13,6 @@ function loadFromDisk() {
     newQueue.messages = messages;
     queues[key] = newQueue;
   });
-
   console.log('+...done.');
 }
 
@@ -23,7 +22,7 @@ function dumpToDisk() {
     if (err) {
       return console.log(err);
     }
-    console.log('+++ dumped queue data to disk');
+    return console.log('+++ dumped queue data to disk');
   });
 }
 
@@ -33,86 +32,95 @@ function respond(sock, response) {
   console.log('  ^   ' + response.trim());
 };
 
-loadFromDisk();
+function declareQueue(data, sock) {
+  if (typeof data[1] === 'undefined') {
+    respond(sock, 'ERR nothing to declare\n');
+  } else if (typeof queues[data[1].trim()] !== 'undefined') {
+    respond(sock, 'ERR queue already declared\n');
+  } else {
+    sock.name = data[1].trim();
+    queues[sock.name] = new queue.Queue(sock.name);
+    respond(sock, 'QOK ' + sock.name + '\n');
+  }
+}
 
-server = net.createServer(function(sock) {
-  sock.on('data', function(d) {
-    sock.setKeepAlive(true);
-    d.toString().split('\n').forEach(function(data) { 
-      var q;
-      var response;
-      
-      console.log('> ' + data.toString().trim());
-      if (process.env.DEBUG) {
-        console.log(data.toString().split(' '));
-        console.log(queues);
-        sock.destroy();
-      }
-      
-      data = data.toString().split(' ');
-      data[0] = data[0].trim();
-      
-      /* declare queue */
-      if (data[0] === 'DCQ') {
-        if (typeof data[1] === 'undefined') {
-          respond(sock, 'ERR nothing to declare\n');
-        } else if (typeof queues[data[1].trim()] !== 'undefined') {
-          respond(sock, 'ERR queue already declared\n');
-        } else {
-          sock.name = data[1].trim();
-          queues[sock.name] = new queue.Queue(sock.name);
-          respond(sock, 'QOK ' + sock.name + '\n');
-        }
-      }
-      
-      /* enqueue */
-      if (data[0] === 'ENQ') {
-        data.shift();
-        if (typeof data[0] === 'undefined' ||
-            typeof queues[data[0].trim()] === 'undefined') {
-          respond(sock, 'ERR nonexistent queue\n');
-        } else if (data.length < 2) {
-          respond(sock, 'ERR nothing to enqueue\n');
-        } else {
-          data[0] = data[0].trim();
-          q = queues[data[0]];
-          data.shift();
-          data = data.join(' ').trim();
-          
-          q.enqueue(data);
-          respond(sock, 'EOK ' + q.messages.length + ' ' + data + '\n');
-        }
-      }
-      
-      /* dequeue */
-      if (data[0] === 'DEQ') {
-        data.shift();
+function enqueue(data, sock) {
+  data.shift();
+  if (typeof data[0] === 'undefined' ||
+      typeof queues[data[0].trim()] === 'undefined') {
+    respond(sock, 'ERR nonexistent queue\n');
+  } else if (data.length < 2) {
+    respond(sock, 'ERR nothing to enqueue\n');
+  } else {
+    data[0] = data[0].trim();
+    var q = queues[data[0]];
+    data.shift();
+    data = data.join(' ').trim();
+    
+    q.enqueue(data);
+    respond(sock, 'EOK ' + q.messages.length + ' ' + data + '\n');
+  }
+}
+
+function dequeue(data, sock) {
+  data.shift();
+  
+  if (typeof data[0] === 'undefined' ||
+      typeof queues[data[0].trim()] === 'undefined') {
+    respond(sock, 'ERR nonexistent queue\n');
+  } else {
+    data[0] = data[0].trim();
+    var q = queues[data[0]];
+    
+    if (q.messages.length < 1) {
+      respond(sock, "NULL empty queue\n");
+    } else {
+      var item = q.dequeue();
+      respond(sock, 'DOK ' + q.messages.length + ' ' + item + '\n');
+    }
+  }
+}
+
+function startServer() {
+  loadFromDisk();
+  
+  server = net.createServer(function(sock) {
+    sock.on('data', function(d) {
+      sock.setKeepAlive(true);
+      d.toString().split('\n').forEach(function(data) { 
+        var q;
+        var response;
         
-        if (typeof data[0] === 'undefined' ||
-            typeof queues[data[0].trim()] === 'undefined') {
-          respond(sock, 'ERR nonexistent queue\n');
-        } else {
-          data[0] = data[0].trim();
-          q = queues[data[0]];
-          
-          if (q.messages.length < 1) {
-            respond(sock, "NULL empty queue\n");
-          } else {
-            var item = q.dequeue();
-            respond(sock, 'DOK ' + q.messages.length + ' ' + item + '\n');
-          }
+        console.log('> ' + data.toString().trim());
+        if (process.env.DEBUG) {
+          console.log(data.toString().split(' '));
+          console.log(queues);
+          sock.destroy();
         }
-      }
+        
+        data = data.toString().split(' ');
+        data[0] = data[0].trim();
+        
+        if (data[0] === 'DCQ') {
+          declareQueue(data, sock);
+        } else if (data[0] === 'ENQ') {
+          enqueue(data, sock);
+        } else if (data[0] === 'DEQ') {
+          dequeue(data, sock);
+        }
+      });
+    });
+    
+    sock.on('error', function(err) {
+      console.log('X ' + err);
+      sock.destroy();
     });
   });
 
-  sock.on('error', function(err) {
-    console.log('X ' + err);
-    sock.destroy();
-  });
-});
+  server.timeout = 0;
+  server.listen(9090);
+  
+  setInterval(dumpToDisk, 60 * 1000);
+}
 
-server.timeout = 0;
-server.listen(9090);
-
-setInterval(dumpToDisk, 60 * 1000);
+startServer();
