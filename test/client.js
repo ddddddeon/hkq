41b7ef5ab603596076f284e745cb3b5407dca31b
ddddddeon <chris@sweetiq.com>
2016-03-27 00:11:33 -0400

    clean up client test

--- test/client.js
+++ test/client.js
@@ -1,20 +1,10 @@
 var Server = require('../index').Server;
 var Client = require('../index').Client;
 
-var config = {
-  "dumpPath": "config/queues.json",
-  "dumpInterval": 1,
-  "listenPort": "1337"
-};
-
-var server = new Server(config);
-server.startServer();
-
 var conf = {
-  "host": "127.0.0.1",
-  "port": 9090
+  'port': 9090,
+  'host': 'localhost'
 };
-
 
 setTimeout(function() {
   var client = new Client(conf);
