dc6e4449f679f143216b1e8885801620fb122950
ddddddeon <chris@sweetiq.com>
2016-03-27 04:08:02 -0400

    return on error in client test

--- test/client.js
+++ test/client.js
@@ -8,16 +8,16 @@ var conf = {
 var client = new Client(conf);
 
 client.enqueue("test", "first message", function(err, data) {
-  if (err) console.log(err);
+  if (err) return console.log(err);
   console.log(data);
   client.enqueue("test", "second message", function(err, data) {
-    if (err) console.log(err);
+    if (err) return console.log(err);
     console.log(data);
     client.dequeue("test", function(err, data) {
-      if (err) console.log(err);
+      if (err) return console.log(err);
       console.log(data);
       client.dequeue("test", function(err, data) {
-        if (err) console.log(err);
+        if (err) return console.log(err);
         console.log(data);
       });
     });
