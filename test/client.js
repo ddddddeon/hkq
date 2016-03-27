3ec29dbd0804f8624a31170f5a13d8c5c6faddf6
ddddddeon <chris@sweetiq.com>
2016-03-26 23:35:57 -0400

    add client test

--- test/client.js
+++ test/client.js
@@ -1,3 +1,32 @@
-var client = require('../index').client;
+var Client = require('../index').Client;
 
-console.log(client);
+var conf = {
+  "host": "127.0.0.1",
+  "port": 9090
+};
+
+var client = new Client(conf);
+
+client.declareQueue("test", function(err, data) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(data);
+  }
+
+  client.enqueue("test", "hi there", function(err, data) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log(data);
+    }
+
+    client.dequeue("test", function(err, data) {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log(data);
+      }
+    });
+  });
+});
